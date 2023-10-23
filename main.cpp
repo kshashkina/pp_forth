7ab1b6ac677a8d12c05b19bfb84fccdf59617c03
Katherine Shashkina <kshashkina@kse.org.ua>
2023-10-23 22:38:39 +0300

    added Command line interface(functions, there is need to put everything in the separate class)

--- main.cpp
+++ main.cpp
@@ -1,15 +1,86 @@
 #include <iostream>
 #include <string>
 #include <Windows.h>
-#include "Reader.h"
+#include <fstream>
 #include "Writer.h"
+#include "Reader.h"
+#include <cstdlib>  // For random number generation
+#include <ctime>    // For seeding random number generator
 
 using namespace std;
 
 typedef string (*encrypt_ptr_t)(string, int);
 typedef string (*decrypt_ptr_t)(string, int);
 
+// Function to handle Normal Mode - Encryption
+void HandleNormalEncrypt(Reader& reader, Writer& writer, encrypt_ptr_t encrypt_ptr) {
+    string inputFilePath, outputFilePath;
+    int key;
+
+    cout << "Enter input file path: ";
+    cin.ignore();
+    getline(cin, inputFilePath);
+
+    cout << "Enter output file path: ";
+    getline(cin, outputFilePath);
+
+    cout << "Enter the key: ";
+    cin >> key;
+
+    string fileContent = reader.Read(inputFilePath);
+    string encryptedText = encrypt_ptr(fileContent, key);
+    writer.Write(outputFilePath, encryptedText);
+
+    cout << "Encryption completed successfully.\n";
+}
+
+// Function to handle Normal Mode - Decryption
+void HandleNormalDecrypt(Reader& reader, Writer& writer, decrypt_ptr_t decrypt_ptr) {
+    string inputFilePath, outputFilePath;
+    int key;
+
+    cout << "Enter input file path: ";
+    cin.ignore();
+    getline(cin, inputFilePath);
+
+    cout << "Enter output file path: ";
+    getline(cin, outputFilePath);
+
+    cout << "Enter the key: ";
+    cin >> key;
+
+    string fileContent = reader.Read(inputFilePath);
+    string decryptedText = decrypt_ptr(fileContent, key);
+    writer.Write(outputFilePath, decryptedText);
+
+    cout << "Decryption completed successfully.\n";
+}
+
+// Function to handle Secret Mode - Encryption with Random Key
+void HandleSecretEncrypt(Reader& reader, Writer& writer, encrypt_ptr_t encrypt_ptr) {
+    string inputFilePath, outputFilePath;
+    int randomKey;
+
+    // Generate a random key
+    srand(time(nullptr));
+    randomKey = rand() % 1000;  // Generate a random key between 0 and 999
+
+    cout << "Enter input file path: ";
+    cin.ignore();
+    getline(cin, inputFilePath);
+
+    cout << "Enter output file path: ";
+    getline(cin, outputFilePath);
+
+    string fileContent = reader.Read(inputFilePath);
+    string encryptedText = encrypt_ptr(fileContent, randomKey);
+    writer.Write(outputFilePath, encryptedText);
+
+    cout << "Encryption completed successfully with a random key: " << randomKey << "\n";
+}
+
 int main() {
+    int number;
     HMODULE handle = LoadLibrary(TEXT("libCaesar.dll"));
 
     if (handle == nullptr) {
@@ -25,42 +96,41 @@ int main() {
         return 1;
     }
 
-    string rawText;
-    int key;
-    cout<< "Write your text"<< endl;
-    getline(cin, rawText);
-    cout << "Write your key"<<endl;
-    cin>>key;
-
-    string encryptedText = encrypt_ptr(rawText, key);
-    string decryptedText = decrypt_ptr(encryptedText, key);
-
-    cout << "Original Text: " << rawText << endl;
-    cout << "Encrypted Text: " << encryptedText << endl;
-    cout << "Decrypted Text: " << decryptedText << endl;
-
-    FreeLibrary(handle);
-
     Reader reader;
-    string filePath;
-    cout<<"Provide file path"<<endl;
-    cin.ignore();
-    getline(cin, filePath);
-
-    string fileContent = reader.Read(filePath);
-    cout << "File content:\n" << fileContent;
-
     Writer writer;
-    string text;
-    string writePath;
-    cout<<"Write your text:"<<endl;
-    cin.ignore();
-    getline(cin, text);
-    cout<<"Write name of the file to put text:"<<endl;
-    cin.ignore();
-    getline(cin, writePath);
 
-    writer.Write(writePath, text);
+    while (true) {
+        cout << "What do you want to do?\n";
+        cout << "1 - Normal mode\n";
+        cout << "2 - Secret mode\n";
+        cout << "3 - Exit\n";
+        cin >> number;
 
-    return 0;
+        switch (number) {
+            case 1: // Normal mode
+                int chosenOperation;
+                cout << "Choose the operation:\n";
+                cout << "1 - Encrypt\n";
+                cout << "2 - Decrypt\n";
+                cin >> chosenOperation;
+
+                switch (chosenOperation) {
+                    case 1:
+                        HandleNormalEncrypt(reader, writer, encrypt_ptr);
+                        break;
+                    case 2:
+                        HandleNormalDecrypt(reader, writer, decrypt_ptr);
+                        break;
+                    default:
+                        break;
+                }
+                break;
+
+            case 2: // Secret mode
+                HandleSecretEncrypt(reader, writer, encrypt_ptr);
+                break;
+            default:
+                return 0;
+        }
+    }
 }

5849cd3016fdc45a75f9949d5fef2c5d29e957cc
Katherine Shashkina <kshashkina@kse.org.ua>
2023-10-23 23:43:55 +0300

    calling funcs in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,83 +1,16 @@
 #include <iostream>
 #include <string>
 #include <Windows.h>
-#include <fstream>
 #include "Writer.h"
 #include "Reader.h"
-#include <cstdlib>  // For random number generation
-#include <ctime>    // For seeding random number generator
+#include "NormalMode.h"
+#include "SecretMode.h"
 
 using namespace std;
 
 typedef string (*encrypt_ptr_t)(string, int);
 typedef string (*decrypt_ptr_t)(string, int);
 
-// Function to handle Normal Mode - Encryption
-void HandleNormalEncrypt(Reader& reader, Writer& writer, encrypt_ptr_t encrypt_ptr) {
-    string inputFilePath, outputFilePath;
-    int key;
-
-    cout << "Enter input file path: ";
-    cin.ignore();
-    getline(cin, inputFilePath);
-
-    cout << "Enter output file path: ";
-    getline(cin, outputFilePath);
-
-    cout << "Enter the key: ";
-    cin >> key;
-
-    string fileContent = reader.Read(inputFilePath);
-    string encryptedText = encrypt_ptr(fileContent, key);
-    writer.Write(outputFilePath, encryptedText);
-
-    cout << "Encryption completed successfully.\n";
-}
-
-// Function to handle Normal Mode - Decryption
-void HandleNormalDecrypt(Reader& reader, Writer& writer, decrypt_ptr_t decrypt_ptr) {
-    string inputFilePath, outputFilePath;
-    int key;
-
-    cout << "Enter input file path: ";
-    cin.ignore();
-    getline(cin, inputFilePath);
-
-    cout << "Enter output file path: ";
-    getline(cin, outputFilePath);
-
-    cout << "Enter the key: ";
-    cin >> key;
-
-    string fileContent = reader.Read(inputFilePath);
-    string decryptedText = decrypt_ptr(fileContent, key);
-    writer.Write(outputFilePath, decryptedText);
-
-    cout << "Decryption completed successfully.\n";
-}
-
-// Function to handle Secret Mode - Encryption with Random Key
-void HandleSecretEncrypt(Reader& reader, Writer& writer, encrypt_ptr_t encrypt_ptr) {
-    string inputFilePath, outputFilePath;
-    int randomKey;
-
-    // Generate a random key
-    srand(time(nullptr));
-    randomKey = rand() % 1000;  // Generate a random key between 0 and 999
-
-    cout << "Enter input file path: ";
-    cin.ignore();
-    getline(cin, inputFilePath);
-
-    cout << "Enter output file path: ";
-    getline(cin, outputFilePath);
-
-    string fileContent = reader.Read(inputFilePath);
-    string encryptedText = encrypt_ptr(fileContent, randomKey);
-    writer.Write(outputFilePath, encryptedText);
-
-    cout << "Encryption completed successfully with a random key: " << randomKey << "\n";
-}
 
 int main() {
     int number;
@@ -99,6 +32,10 @@ int main() {
     Reader reader;
     Writer writer;
 
+    NormalMode normalMode(reader, writer, encrypt_ptr, decrypt_ptr);
+    SecretMode secretMode(reader, writer, encrypt_ptr);
+
+
     while (true) {
         cout << "What do you want to do?\n";
         cout << "1 - Normal mode\n";
@@ -116,10 +53,10 @@ int main() {
 
                 switch (chosenOperation) {
                     case 1:
-                        HandleNormalEncrypt(reader, writer, encrypt_ptr);
+                        normalMode.HandleEncrypt();
                         break;
                     case 2:
-                        HandleNormalDecrypt(reader, writer, decrypt_ptr);
+                        normalMode.HandleDecrypt();
                         break;
                     default:
                         break;
@@ -127,7 +64,7 @@ int main() {
                 break;
 
             case 2: // Secret mode
-                HandleSecretEncrypt(reader, writer, encrypt_ptr);
+                secretMode.HandleSecretEncrypt();
                 break;
             default:
                 return 0;
